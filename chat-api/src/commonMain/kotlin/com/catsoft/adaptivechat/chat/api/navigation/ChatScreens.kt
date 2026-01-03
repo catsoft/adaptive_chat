4245ff6e280456a9f54f13ea4ea299e30d609169
Aleksandr Vakhtin <avakhtin@chatfuel.com>
2026-01-03 19:22:30 +0000

    Build src

--- chat-api/src/commonMain/kotlin/com/catsoft/adaptivechat/chat/api/navigation/ChatScreens.kt
+++ chat-api/src/commonMain/kotlin/com/catsoft/adaptivechat/chat/api/navigation/ChatScreens.kt
@@ -1,5 +1,10 @@
 package com.catsoft.adaptivechat.chat.api.navigation
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed class ChatScreens {
+
+    @Serializable
     data class Chat(val conversationId: String, val agentId: String): ChatScreens()
 }
