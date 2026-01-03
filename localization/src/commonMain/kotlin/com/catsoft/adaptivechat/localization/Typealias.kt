c46a697492d76d472abac8e4ecdf339152e73cf7
Aleksandr Vakhtin <avakhtin@chatfuel.com>
2026-01-03 20:12:58 +0000

    Localization

---
+++ localization/src/commonMain/kotlin/com/catsoft/adaptivechat/localization/Typealias.kt
@@ -0,0 +1,10 @@
+package com.catsoft.adaptivechat.localization
+
+import com.adaptivechat.localization.resources.Res
+
+
+typealias Strings = Res.string
+
+typealias Drawables = Res.drawable
+
+typealias CFFont = Res.font
