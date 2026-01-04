47f382ea9c6d5fe5999ac0c2f0de0610c31bbea8
Aleksandr Vakhtin <avakhtin@chatfuel.com>
2026-01-04 18:52:34 +0000

    Chat

--- ui-kit/src/commonMain/kotlin/com/catsoft/adaptivechat/ui/kit/di/UIKitModule.kt
+++ ui-kit/src/commonMain/kotlin/com/catsoft/adaptivechat/ui/kit/di/UIKitModule.kt
@@ -1,8 +1,10 @@
 package com.catsoft.adaptivechat.ui.kit.di
 
+import com.catsoft.adaptivechat.ui.kit.connectionStatus.ConnectivityViewModel
 import com.catsoft.adaptivechat.ui.kit.topBar.TopBarVisibilityState
 import com.catsoft.adaptivechat.ui.kit.topBar.TopBarVisibilityStateImpl
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -10,5 +12,5 @@ import org.koin.dsl.module
 val uiKitModule = module {
     singleOf(::TopBarVisibilityStateImpl) bind TopBarVisibilityState::class
 
-
+    viewModelOf(::ConnectivityViewModel)
 }
