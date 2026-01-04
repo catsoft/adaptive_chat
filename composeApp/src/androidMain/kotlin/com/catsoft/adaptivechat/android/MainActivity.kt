4199db0aac7ae916e79b83e228575e1bf318a85d
Aleksandr Vakhtin <avakhtin@chatfuel.com>
2026-01-04 20:14:28 +0000

    Themes

--- composeApp/src/androidMain/kotlin/com/catsoft/adaptivechat/android/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/catsoft/adaptivechat/android/MainActivity.kt
@@ -2,15 +2,8 @@ package com.catsoft.adaptivechat.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.material.Colors
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import com.catsoft.adaptivechat.App
-import com.catsoft.adaptivechat.ui.kit.modifier.colors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
